2fbb9d3c43bbfe6a4524b2c7fc0fa84f15945393
Tim Caswell <tim@creationix.com>
2014-03-25 12:10:03 +0000

    Save Progress

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ window.requestAnimationFrame =
   window.webkitRequestAnimationFrame;
 
 window.addEventListener("polymer-ready", function () {
-  var ticker = new CreationixTicker();
+  var ticker = new window.CreationixTicker();
   document.body.appendChild(ticker);
+  ticker.addMessage("The laptop is orange");
+  ticker.addMessage("This is a ticker");
+  ticker.addMessage("It's cool right?!");
   setInterval(function () {
-    ticker.maxItems = Date.now();
-  }, 500);
+    ticker.addMessage("Message " + Date.now());
+  }, 4000);
 }, false);
--- elements/creationix-ticker.js
+++ elements/creationix-ticker.js
@@ -1,16 +1,63 @@
 /*global Polymer*/
-Polymer('creationix-ticker', {
-  maxItems: 10,
-  ready: function () {
-    var messages = [];
-    var currentMessage = 0;
+(function () {
+  var width;
 
+  Polymer('creationix-ticker', {
+    maxItems: 10,
+    ready: function () {
+      var messages = this.messages = [];
+      this.messageIndex = 0;
+      setInterval(onFrame, 30);
+      var prev = 0;
+      var num = 0;
+      function onFrame() {
+        var now = Date.now();
+        var width = window.innerWidth;
+        messages.forEach(function (item) {
+          if (!item.active && num && prev > 0) {
+            reset(item);
+          }
+          else {
+            if (!item.active) {
+              item.start = now;
+              item.active = true;
+              num++;
+            }
+          }
+          var delta = now - item.start;
+          var offset = width * (1 - delta / 5000);
+          item.el.style.webkitTransform = "translateX(" + offset + "px)";
+          var selfWidth = item.el.offsetWidth;
+          prev = offset - width + selfWidth;
+          if (offset < -1 * selfWidth) {
+            reset(item);
+          }
+        });
 
-    window.requestAnimationFrame(onFrame);
-    function onFrame() {
-
-      window.requestAnimationFrame(onFrame);
+        function reset(item) {
+          item.start = now;
+          if (item.active) {
+            item.active = false;
+            num--;
+          }
+          item.start = now;
+        }
+      }
+    },
+    addMessage: function (message) {
+      var item = {
+        el: document.createElement("li"),
+        start: Date.now()
+      };
+      this.messages.sort(function (a, b) {
+        return a.start - b.start;
+      });
+      item.el.textContent = message;
+      this.messages.push(item);
+      this.shadowRoot.appendChild(item.el);
     }
-  }
 
-});
+  });
+
+
+}());
